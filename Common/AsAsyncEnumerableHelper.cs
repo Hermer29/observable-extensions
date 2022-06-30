f980808a305922f5e334c9a0aa6948bd7f5d9dc6
Hermer29 <kliman.chan@gmail.com>
2022-06-30 18:00:11 +0300

    feat(common): add AsAsyncEnumerable extension method

---
+++ Common/AsAsyncEnumerableHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace Hermer29.ObservableExtensions
+{
+    internal class AsAsyncEnumerableHelper<T> : IAsyncEnumerable<T>
+    {
+        private readonly IObservable<T> _observable;
+
+        public AsAsyncEnumerableHelper(IObservable<T> observable)
+        {
+            _observable = observable;
+        }
+
+        public async IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            bool newValueAppeared = false;
+            T newValue = default;
+            _observable.Subscribe(x =>
+            {
+                newValue = x;
+                newValueAppeared = true;
+            });
+
+            while (true)
+            {
+                await Task.Run(() =>
+                {
+                    while (newValueAppeared == false)
+                    {
+                        if (cancellationToken.IsCancellationRequested)
+                            return;
+                    }
+                    newValueAppeared = false;
+                });
+                if (cancellationToken.IsCancellationRequested)
+                    yield break;
+                yield return newValue;
+            }
+        }
+    }
+}
---
+++ Common/ConvertationExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Hermer29.ObservableExtensions
+{
+    public static class ConvertationExtensions
+    {
+        public static IAsyncEnumerable<T> AsAsyncEnumerable<T>(this IObservable<T> observable)
+        {
+            return new AsAsyncEnumerableHelper<T>(observable);
+        }
+    }
+}
