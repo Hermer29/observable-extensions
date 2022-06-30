84fa0d1aa65a9c0c1668be3eecc95c9de6f127f8
Hermer29 <kliman.chan@gmail.com>
2022-06-30 17:18:09 +0300

    feat(common): add subscription container
    ObserversPool reduces boilerplate in observables implementation process

---
+++ Common/ObserversPool.cs
@@ -0,0 +1,33 @@
+﻿namespace Hermer29.ObservableExtensions
+{
+    public class ObserversPool<T> : IDisposable
+    {
+        private Dictionary<object, IObserver<T>> _observers = new Dictionary<object, IObserver<T>>();
+
+        public IDisposable AddObserver(IObserver<T> observer)
+        {
+            var id = new object();
+            _observers.Add(id, observer);
+            var disposable = Disposable.Create(id, OnSubscriptionDisposed);
+            return disposable;
+        }
+
+        private void OnSubscriptionDisposed(object id)
+        {
+            _observers.Remove(id);
+        }
+
+        public void Dispose()
+        {
+            _observers.Clear();
+        }
+
+        public void NotifyAll(T value)
+        {
+            foreach(var subscriber in _observers.Values)
+            {
+                subscriber.OnNext(value);
+            }
+        }
+    }
+}
